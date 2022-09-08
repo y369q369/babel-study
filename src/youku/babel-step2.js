d8ec3191daf22ff6556d8417375feeb17d08f158
gs <1486866853@qq.com>
2022-09-08 19:33:25 +0800

    ifelse转换

--- src/youku/babel-step2.js
+++ src/youku/babel-step2.js
@@ -1,8 +1,5 @@
-// 第一步处理：
-//      1. 将三元表达式 转换为 if - else 语句
-//      2. 去除 !function(){}() 语句
-//      3. if else 添加 {}
-//      4. 16进制 、 unicode字符 转为正常字符
+// 第二步处理：
+//      1. if else 转 switch
 
 const parser = require('@babel/parser')
 const generate = require('@babel/generator').default
@@ -11,154 +8,226 @@ const t = require('@babel/types')
 const fs = require('fs');
 const path = require('path');
 
-const demo1 = fs.readFileSync(path.join(__dirname, './collina.js'), {
+const demo1 = fs.readFileSync(path.join(__dirname, './collina-step1.js'), {
     encoding: 'utf-8'
 });
 
+
+
+// && 逻辑表达式转 if - else
+// const demo1 = `Ai > 0 && (L = mo, li = 24641)
+// Ai > 0 && L
+// !ie[he] && !Q
+// `
+
 // 三元表达式示例
-// const demo1 = `function demo() {
-//     var a2
-//     // 三元表达式赋值的不处理
-//     a2 = 1 == 1 ? 3 : 4;
-//     li = 17 > Ai ? Ao < H.length ? 2629 : 12866 : ee ? 642 : 4581
-//     1 == 1 ? (console.log(1), a2 = 5) : 2 == 2 ? 456 : 123
-//     if (1 == 1) {
-//         console.log(1)
+// const demo1 = `if (12 == Ai) {
+//     N = Se[vo];
+// } else if (12 > Ai) {
+//     if (5 == Ai) {
+//         li = 8804;
+//     } else if (5 > Ai) {
+//         if (2 == Ai) {
+//             ie = [];
+//         } else if (2 > Ai) {
+//             if (0 == Ai) {
+//                 Dn.push(0);
+//                 li = 11522;
+//             } else if (Ai > 0) {
+//                 L = mo;
+//                 li = 24641;
+//             }
+//         } else {
+//             if (3 == Ai) {
+//                 Oe = K;
+//                 li = 20257;
+//             } else if (Ai > 3) {
+//                 _ = 0 !== se.length;
+//                 I = je;
+//                 li = _ ? 22694 : 16963;
+//             }
+//         }
 //     } else {
-//         console.log(2)
+//         if (8 == Ai) {
+//             li = Pn < qe.length ? 22722 : 23586;
+//         } else if (8 > Ai) {
+//             if (6 == Ai) {
+//                 je += "u";
+//             } else if (Ai > 6) {
+//                 y = d;
+//             }
+//         } else {
+//             if (10 == Ai) {
+//                 li = ie[le] ? 5218 : 1185;
+//             } else if (10 > Ai) {
+//                 on = [];
+//             } else {
+//                 mn |= I;
+//             }
+//         }
+//     }
+// } else {
+//     if (19 == Ai) {
+//         li = 1313;
+//     } else if (19 > Ai) {
+//         if (15 == Ai) {
+//             $ = po;
+//             li = 26147;
+//         } else if (15 > Ai) {
+//             if (13 == Ai) {
+//                 Ue = on;
+//                 li = Ue ? 3683 : 3206;
+//             } else if (Ai > 13) {
+//                 li = co ? 12834 : 3808;
+//             }
+//         } else {
+//             if (17 == Ai) {
+//                 Sn = ee[7];
+//             } else if (17 > Ai) {
+//                 m = T[zt](u[27], Oe);
+//                 li = 20996;
+//             } else {
+//                 li = Qe < X.length ? 1633 : 8641;
+//             }
+//         }
+//     } else {
+//         if (22 == Ai) {
+//             li = fe < Xe.length ? 22243 : 25313;
+//         } else if (22 > Ai) {
+//             if (20 == Ai) {
+//                 li = I < g.length ? 25092 : 7650;
+//             } else if (Ai > 20) {
+//                 Le = Ao.indexOf(se);
+//             }
+//         } else {
+//             if (24 == Ai) {
+//                 ye = he - 1;
+//                 li = 16162;
+//             } else if (24 > Ai) {
+//                 Ie += "tN";
+//                 li = On ? 26337 : 1568;
+//             } else {
+//                 mn++;
+//                 li = 17509;
+//             }
+//         }
 //     }
 // }`
 
-// !function示例
-// const demo1 = `!function() {
-//     var u
-//     switch (u) {
-//         case 1:
-//             !function() {
-//                 console.log(1)
-//                 console.log(2)
-//                 console.log(3)
-//             }()
-//             break
-//         case 2:
-//             console.log(2)
-//         default:
-//             console.log('default')
-//     }
-// }()`
 
-// if else 示例
-// const demo1 = `if (12 == Ai)
-//     ae = ce << F
-// else if (12 > Ai)
-//     5 == Ai
-//
-// if (19 == Ai)
-//    M++, li = 8229;
-// else if (19 > Ai) {
-//     li = 24741;
-// } else
-//   456
-// `
 
-// unicode编码 和 十六进制编码 示例
-// const demo1 = `var t1 = '\x4a\x63\x4f\x46'
-// var t2 = '\u0175\u0170\u0154\u0175\u0173\u016a\u016f\u0168'
-// var t3 = 'qwe'
-// `
+
+const numArr = [0, 3, 6, 13, 20, 10, 17, 24]
 
 
 const astDemo1 = parser.parse(demo1)
 
-// 判断是否为赋值表达式的三元表达式， flase: 是， true: 不是
-const isTransformConditionalExpression = function (parentPath) {
-    if (parentPath.node.type === 'ConditionalExpression') {
-        return isTransformConditionalExpression(parentPath.parentPath)
-    } else if (parentPath.node.type != 'AssignmentExpression') {
-        return true
-    }
-    return false
-}
 
 const visitor = {
     // 针对三元表达式类型处理
-    ConditionalExpression(path) {
-        if (isTransformConditionalExpression(path.parentPath)) {
-            // if (path.parentPath.type != 'AssignmentExpression') {
-            // 将三元表达式类型转为 if-else类型
-            path.node.type = 'IfStatement'
-
-            // 左半部分 添加{}
-            let tempConsequent = t.blockStatement([])
-            if (path.node.consequent.type === 'SequenceExpression') {
-                tempConsequent.body = path.node.consequent.expressions
-            } else {
-                tempConsequent.body.push(path.node.consequent)
+    LogicalExpression(path) {
+        // 将 &&逻辑表达式 转为 if-else类型
+        if ((path.node.operator === '&&') && (path.node.left.type === 'BinaryExpression')) {
+            if (path.parentPath.node.type == 'ExpressionStatement') {
+                let tempBlock
+                if (path.node.right.type == 'SequenceExpression') {
+                    let blockStatementBody = []
+                    path.node.right.expressions.forEach(expression => {
+                        if (expression.type !== 'ExpressionStatement') {
+                            blockStatementBody.push(t.expressionStatement(expression))
+                        } else {
+                            blockStatementBody.push(expression)
+                        }
+                    })
+                    tempBlock = t.blockStatement(blockStatementBody)
+                } else {
+                    tempBlock = t.blockStatement([t.expressionStatement(path.node.right)])
+                }
+                let tempIfStatement = t.ifStatement(path.node.left, tempBlock)
+                path.parentPath.replaceWith(tempIfStatement)
             }
-            path.node.consequent = tempConsequent
-
-            // 右半部分 添加{}
-            let tempAlternate = t.blockStatement([])
-            if (path.node.alternate.type === 'SequenceExpression') {
-                tempAlternate.body = path.node.alternate.expressions
-            } else {
-                tempAlternate.body.push(path.node.alternate)
-            }
-            path.node.alternate = tempAlternate
         }
     },
 
-    // 针对 !function 处理
-    UnaryExpression(path) {
-        if (path.node.argument.type === 'CallExpression') {
-            if (path.node.argument.callee.type === 'FunctionExpression' && path.node.argument.callee.body.type === 'BlockStatement') {
-                let node = path.parentPath
-                // 将 !function 下方代码替换到当前的父结点上
-                node.replaceWithMultiple(path.node.argument.callee.body.body)
-
-                // 拼接子节点
-                // if (node.type === 'Program') {
-                //     node.body.push.apply(node.body, path.node.argument.callee.body.body)
-                // } else if (node.type === 'SwitchCase') {
-                //     node.consequent.push.apply(node.consequent, path.node.argument.callee.body.body)
-                // }
-                // 删除当前父级节点
-                // path.parentPath.remove()
-            }
-
-        }
-    },
-
-    // 将 if-else 添加 {}
+    // if else 处理
     IfStatement(path) {
-        if (path.node.consequent.type != 'BlockStatement') {
-            let tempConsequent = t.blockStatement([path.node.consequent])
-            path.node.consequent = tempConsequent
-        }
-        if (path.node.alternate && path.node.alternate.type != 'BlockStatement') {
-            if (path.node.alternate.type != 'BlockStatement' && path.node.alternate.type != 'IfStatement') {
-                let tempConsequent = t.blockStatement([path.node.alternate])
-                path.node.alternate = tempConsequent
+        if (path.node.test.type == 'BinaryExpression') {
+            if (path.node.test.left.type == 'Identifier' && path.node.test.left.name == 'Ai' && path.node.test.operator == '>' && path.node.test.right.type == 'NumericLiteral') {
+                if (numArr.includes(path.node.test.right.value)) {
+                    if (path.node.test.right.value == 0) {
+                        path.node.test.right.value = 1
+                    } else if (path.node.test.right.value == 3) {
+                        path.node.test.right.value = 4
+                    } else if (path.node.test.right.value == 6) {
+                        path.node.test.right.value = 7
+                    } else if (path.node.test.right.value == 13) {
+                        path.node.test.right.value = 14
+                    } else if (path.node.test.right.value == 20) {
+                        path.node.test.right.value = 21
+                    }
+
+                    path.node.test.operator = '=='
+                    let tempNode = path.node.test.right
+                    path.node.test.right = path.node.test.left
+                    path.node.test.left = tempNode
+                }
+            } else if (path.node.test.right.type == 'Identifier' && path.node.test.right.name == 'Ai' && path.node.test.operator == '>' && path.node.test.left.type == 'NumericLiteral') {
+                if (numArr.includes(path.node.test.left.value)) {
+                    if (path.node.test.left.value == 10) {
+                        path.node.test.left.value = 9
+
+                        let tempBinaryExpression = t.binaryExpression('==', t.identifier('Ai'), t.numericLiteral(11))
+                        let tempIfStatement = t.ifStatement(tempBinaryExpression, path.node.alternate, null)
+                        path.node.alternate = tempIfStatement
+                    } else if (path.node.test.left.value == 17) {
+                        path.node.test.left.value = 16
+
+                        let tempBinaryExpression = t.binaryExpression('==', t.identifier('Ai'), t.numericLiteral(18))
+                        let tempIfStatement = t.ifStatement(tempBinaryExpression, path.node.alternate, null)
+                        path.node.alternate = tempIfStatement
+                    } else if (path.node.test.left.value == 24) {
+                        path.node.test.left.value = 23
+                    }
+
+                    path.node.test.operator = '=='
+                    let tempNode = path.node.test.right
+                    path.node.test.right = path.node.test.left
+                    path.node.test.left = tempNode
+                }
             }
         }
     },
 
-    // 将所有十六进制编码与Unicode编码转为正常字符
-    // StringLiteral(path) {
-    //     // rawValue 属性 赋值给 raw 属性
-    //     path.node.extra.raw = "\"" + path.node.extra.rawValue + "\"";
-    // },
-
+    // 去除空语句
+    // EmptyStatement(path) {
+    //     path.remove()
+    // }
 }
 
 traverse(astDemo1, visitor)
 
 const transformedCode = generate(astDemo1).code
-fs.writeFile('./youku/step1.js', transformedCode, function (err) {
+
+fs.writeFile('./step2.js', transformedCode, function (err) {
     if (err) {
-        console.error('三元表达式 转换 if-else 语句 写入文件异常', err)
+        console.error('三元表达式 转换2 写入文件异常', err)
+    } else {
+        console.log("三元表达式 转换2 写入 collina-step2.js 成功！");
     }
-    console.log("三元表达式 转换 if-else 语句 写入 collina-step1.js 成功！");
 })
 
 // console.log(transformedCode)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
